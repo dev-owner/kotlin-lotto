3245f877ccf8066d7780d3229bd6698168e313d4
dev-owner <yureka1112@gmail.com>
2021-03-15 23:57:47 +0900

    refactor(lotto): 두번째 리뷰 반영

--- src/test/kotlin/lotto/domain/LottoStoreTest.kt
+++ src/test/kotlin/lotto/domain/LottoStoreTest.kt
@@ -16,7 +16,7 @@ internal class LottoStoreTest {
         // when
         val lottos = LottoStore().buy(money)
         // then
-        assertThat(lottos.getLottoCount()).isEqualTo(expected)
+        assertThat(lottos.size).isEqualTo(expected)
     }
 
     @Test
